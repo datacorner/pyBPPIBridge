c71efafbcc40a94d465ddde44f2ccc240914cdfb
datacorner <benoit@datacorner.fr>
2023-06-19 14:09:55 +0200

    new SQL

--- odbc.sql
+++ odbc.sql
@@ -1,5 +1,2 @@
-SELECT logid,sessionnumber,stagename,pagename,result,startdatetime, stagetype, 
-        IIF(processname IS NULL, 'BP OBJECT', 'BP PROCESS') as OBJECT_TYPE , 
-        IIF(processname IS NULL, objectname, processname) as OBJECT_NAME 
-FROM BPASessionLog_NonUnicode 
-WHERE sessionnumber IN (SELECT distinct sessionnumber  FROM BPASessionLog_NonUnicode WHERE processname = 'Bulk IFA Application Process')
+SELECT Event, EventID
+  FROM RefEvents
